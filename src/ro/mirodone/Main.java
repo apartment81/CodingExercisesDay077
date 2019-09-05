3e9d247ed6e264b83db90c0e1e505aefcf9f0bc1
apartment81 <bogdan.mirodone@gmail.com>
2019-09-06 00:40:48 +0300

    primorial

---
+++ src/ro/mirodone/Main.java
@@ -0,0 +1,23 @@
+package ro.mirodone;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Primorial primorial = new Primorial();
+
+        Primorial2 primorial2 = new Primorial2();
+
+        System.out.println(primorial2.numPrimorial(3));
+        System.out.println(primorial2.numPrimorial(4));
+
+
+        int n = 5;
+        primorial.sieveSundaram();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Primorial(P#) of " + i + " is " + primorial.calculatePrimorial(i));
+        }
+
+
+    }
+}
---
+++ src/ro/mirodone/Primorial2.java
@@ -0,0 +1,27 @@
+package ro.mirodone;
+
+public class Primorial2 {
+
+    public String numPrimorial(int n) {
+
+        long primorial = 1, factor = 1;
+
+        while (n != 0) {
+            factor++;
+            boolean isPrime = true;
+            for (int i = 2; i < factor; i++) {
+                if (factor % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime == true) {
+                n--;
+                primorial *= factor;
+            }
+        }
+
+        return primorial + "";
+
+    }
+}
